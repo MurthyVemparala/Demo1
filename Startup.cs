27d37fde3908d2e1bbad5a574b65bc19348215d5
MurthyTechM <murthy.vemparala@techmahindra.com>
2016-07-22 17:55:08 +0530

    Added default MVC project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoTM1.Startup))]
+namespace DemoTM1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
